f889bb88531391894b6ccbbd551393e97f556771
valselviz <valeriaselviz@gmail.com>
2023-06-11 14:40:31 -0300

    adding password icon

--- login.js
+++ login.js
@@ -46,3 +46,21 @@ function createUser(){
         }
     }
 }
+/*
+const triggerPassword = document.querySelector('.fa-eye');
+
+const showPassword = trigger => {
+  trigger.addEventListener('click', () => {
+    if(trigger.previousElementSibling.getAttribute('type') === 'password'){
+      trigger.previousElementSibling.setAttribute('type', 'text');
+      trigger.classList.remove('fa-eye');
+      trigger.classList.add('fa-eye-slash');
+    }else if(trigger.previousElementSibling.getAttribute('type') === 'text'){
+      trigger.previousElementSibling.setAttribute('type', 'password');
+      trigger.classList.remove('fa-eye-slash');
+      trigger.classList.add('fa-eye');
+    }
+  });
+}
+
+showPassword(triggerPassword);*/
