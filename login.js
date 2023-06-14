dfbc58cba1fc55614476ad3c8d836e4a28f479e7
valselviz <valeriaselviz@gmail.com>
2023-06-14 20:27:21 -0300

    login with fetch

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 
-function login(){
+async function login(){
     const inputUserName = document.getElementById("userName")
     const inputUserPassword = document.getElementById("userPassword") 
     const url='http://localhost:3000/login'
@@ -7,63 +7,32 @@ function login(){
         "email": inputUserName.value,
         "password": inputUserPassword.value
     })
-    const loginPromise = fetch(url, {
+
+    const response = await fetch(url, {
         method: 'POST',
         body: body,
         headers: {
             "Content-Type": "application/json"
         }
     })
-    loginPromise.then(response => {
-        console.log(response)
-        const jsonPromise = response.json()
-        jsonPromise.then(json => {
-            console.log(json)
-        })
-    })
+    const json = await response.json()
+    console.log(json)
 
+    if (response.status == 200){ //if the request was successful, continue with login
+        //const response = JSON.parse(Http.responseText)
+        document.cookie = "accessToken=" + json.accessToken
+        // Simulate an HTTP redirect to the home page
+        window.location.replace("/home.html");
+    }
+    else if (response.status == 400){ // if the request failed, show error
+        document.getElementById("errorText").hidden=false
 
+        const passwordDiv = document.getElementById("userPassword").parentNode
+        passwordDiv.className+=" formInputError"
 
-
-
-
-
-
-
-
-/*
-    const Http = new XMLHttpRequest()
-    const url='http://localhost:3000/login'
-    Http.open("POST", url);
-    Http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    Http.send(JSON.stringify({
-        "email": inputUserName.value,
-        "password": inputUserPassword.value
-    }))
-
-    Http.onreadystatechange=function(){
-        if (this.readyState == 4) {
-            console.log(this.status)
-            console.log(Http.responseText)
-            console.log(this)
-            console.log(Http)
-            if (this.status == 200){ //if the request was successful, continue with login
-                const response = JSON.parse(Http.responseText)
-                document.cookie = "accessToken=" + response.accessToken
-                // Simulate an HTTP redirect to the home page
-                window.location.replace("/home.html");
-            }
-            else if (this.status == 400){ // if the request failed, show error
-                document.getElementById("errorText").hidden=false
-
-                const passwordDiv = document.getElementById("userPassword").parentNode
-                passwordDiv.className+=" formInputError"
-
-                const nameDiv = document.getElementById("userName").parentNode
-                nameDiv.className+=" formInputError"
-            }
-        }
-    }*/
+        const nameDiv = document.getElementById("userName").parentNode
+        nameDiv.className+=" formInputError"
+    }
 }
 
 function createUser(){
