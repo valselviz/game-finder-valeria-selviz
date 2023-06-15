7cb6f03c44a6152e34b5eb9d82a06872e9797b70
valselviz <valeriaselviz@gmail.com>
2023-06-14 21:59:27 -0300

    validate password

--- login.js
+++ login.js
@@ -4,8 +4,8 @@ async function login(){
     const inputUserPassword = document.getElementById("userPassword") 
     const url='http://localhost:3000/login'
     const body = JSON.stringify({
-        "email": inputUserName.value,
-        "password": inputUserPassword.value
+        email: inputUserName.value,
+        password: inputUserPassword.value
     })
 
     const response = await fetch(url, {
@@ -35,23 +35,63 @@ async function login(){
     }
 }
 
-function createUser(){
-    const Http = new XMLHttpRequest()
-    const url='http://localhost:3000/users/register'
-    Http.open("POST", url);
-    Http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    Http.send(JSON.stringify({
-        "email": "usuario@mail.com",
-        "password": "Pass.4321"
-    }))
+function setErrorMessage(message){
+    const errorParagraph = document.getElementById('errorText')
+    errorParagraph.setHTML(message)
+    errorParagraph.hidden=false
+}
 
-    Http.onreadystatechange=function(){
-        if (this.readyState == 4) {
-            console.log(this.status)
-            console.log(Http.responseText)
-            console.log(this)
-            console.log(Http)
-        }
+function validatePassword(password){
+    
+    //validate the length
+  if (password.length < 8) {
+    setErrorMessage('The password should be at least 8 characters long')
+    return false;
+  } 
+  //validate letter
+  if (!password.match(/[a-z]/)) {
+    setErrorMessage('The password should contain a lowercase letter')
+    return false;
+  } 
+  //validate capital letter
+  if (!password.match(/[A-Z]/)) {
+    setErrorMessage('The password should contain a upercase letter')
+    return false;
+  } 
+  //validate special character
+  if (!password.match(/[!"#$%&'()*+,-.:;<=>?@[\]^_`{|}~]/)) {
+    setErrorMessage('The password should contain special characters')
+    return false;
+  } 
+  //validate number
+  if (!password.match(/\d/)) {
+    setErrorMessage('The password should contain numbers')
+    return false;
+  } 
+  return true
+}
+
+async function createUser(){
+    const inputUserName = document.getElementById("userName")
+    const inputUserPassword = document.getElementById("userPassword") 
+
+    const passwordIsValid = validatePassword(inputUserPassword.value)
+
+    if (passwordIsValid == true){
+        const url='http://localhost:3000/users/register'
+        const body = JSON.stringify({
+            email: inputUserName.value,
+            password: inputUserPassword.value
+        })
+
+        const response = await fetch(url, {
+            method: 'POST',
+            body: body,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const json = await response.json()
     }
 }
 
