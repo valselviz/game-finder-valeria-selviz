3b2139f8452e6b6c4701877549b0f71358da5d94
valselviz <valeriaselviz@gmail.com>
2023-06-19 20:36:20 -0300

    working on games cards

--- js/home.js
+++ js/home.js
@@ -1,5 +1,16 @@
 function createGameCards(gamesData){
-    console.log(gamesData)
+    for(i = 0; i < gamesData.results.length; i++){
+        const gameName = gamesData.results[i].name
+        const gameReleaseDate = gamesData.results[i].released
+        let gameGenres = []
+        for (y = 0; y < gamesData.results[i].genres.length; y++){
+            gameGenres.push(gamesData.results[i].genres[y].name)
+        }
+        createNewCard(gameName, gameReleaseDate, gameGenres, i)
+        console.log(gameName)
+        console.log(gameReleaseDate)
+        console.log(gameGenres)
+    }
 }
 
 function showGames(){
@@ -17,15 +28,19 @@ function showGames(){
     
 }
 
-function createNewCard(){
+function createNewCard(gameName, gameReleaseDate, gameGenres, gameCount){
     //const cardTemplate = document.getElementById("cardTemplate")
-    console.log(cardTemplate)
     const newCard = cardTemplate.cloneNode(true)
-    newCard.id = "game0"
-    console.log(gamesContainer)
+    newCard.id = "game" + gameCount
     gamesContainer.appendChild(newCard)
-    const gameTitleElement = document.querySelector("#game0 .gameTitle")
-    gameTitleElement.innerHTML = "holis"
+    const gameTitleElement = document.querySelector("#" + newCard.id + " .gameTitle")
+    gameTitleElement.innerHTML = gameName
+    const gameRankingElement = document.querySelector("#" + newCard.id + " .ranking")
+    gameRankingElement.innerHTML = "#" + (gameCount + 1)
+    const gameReleaseDateElement = document.querySelector("#" + newCard.id + " .releaseDate")
+    gameReleaseDateElement.innerHTML = gameReleaseDate
+    const gameGenresElement = document.querySelector("#" + newCard.id + " .genres")
+    gameGenresElement.innerHTML = gameGenres.join(", ")
     newCard.hidden = false
 }
 
