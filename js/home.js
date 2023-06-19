b8732475a1821e17cb622c1ed9e8b77ace997b4a
valselviz <valeriaselviz@gmail.com>
2023-06-19 19:32:50 -0300

    processing games data

--- js/home.js
+++ js/home.js
@@ -1,15 +1,21 @@
-/*const requestOptions = {
-    headers: {
-        "Target-URL": "https://rawg.io",
-        "Authorization": "" // the cors-proxy expects this attribute, otherwise it fails
-    }
+function createGameCards(gamesData){
+    console.log(gamesData)
 }
-const responsePromise = fetch("http://localhost:3000/api/games?key=c44227f248074d62bed3059005616f3d", requestOptions)
 
-const jsonPromise = responsePromise.then(resp => resp.json())
-console.log(jsonPromise)
-
-jsonPromise.then(json => console.log(json))*/
+function showGames(){
+    const requestOptions = {
+        headers: {
+            "Target-URL": "https://rawg.io",
+            "Authorization": "" // the cors-proxy expects this attribute, otherwise it fails
+        }
+    }
+    const responsePromise = fetch("http://localhost:3000/api/games?key=c44227f248074d62bed3059005616f3d", requestOptions)
+    
+    const jsonPromise = responsePromise.then(resp => resp.json())
+    
+    jsonPromise.then(createGameCards)
+    
+}
 
 function createNewCard(){
     //const cardTemplate = document.getElementById("cardTemplate")
@@ -24,7 +30,7 @@ function createNewCard(){
 }
 
 const initialization = () => {
-    createNewCard()
+    showGames()
 }
 addEventListener("DOMContentLoaded", initialization)
 
